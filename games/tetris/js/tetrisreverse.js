e88619e9a5d6e96b67704022836cb413df1b55e5
ztgeng <zhtgeng@gmail.com>
2020-10-18 17:51:43 -0700

    tetris reverse cleanup code

--- games/tetris/js/tetrisreverse.js
+++ games/tetris/js/tetrisreverse.js
@@ -1,20 +1,20 @@
 /**
- * @version 0.2
+ * @version 0.3
  * @author Geng
  */
 
 (function() {
-    var BG_COLORS = ["yellow", "aqua", "red", "green", "orange", "blue", "purple"];
-    var ROW_NUM = 20;
-    var COL_NUM = 10;
-    var INIT_C = 3;
-    var NEW_BLOCK_SCORE = 10;
-    var REMOVE_LINE_SCORE = 50;
-    var SPEED_THRESHOLD = 30; //lines
-    var ANIMATE_DURATION = 40; //milliseconds
-    var SPEEDS = [10000, 8000, 5000, 4000];
 
-    var BLOCKS = [
+    const ROW_NUM = 20;
+    const COL_NUM = 10;
+    const INIT_C = 3;
+    const NEW_BLOCK_SCORE = 10;
+    const REMOVE_LINE_SCORE = 50;
+    const SPEED_THRESHOLD = 30; //lines
+    const ANIMATE_DURATION = 40; //milliseconds
+    const SPEEDS = [10000, 8000, 5000, 4000]; //milliseconds
+    const COLORS = ["red", "orange", "blue", "purple", "yellow", "green", "aqua"]
+    const BLOCKS = [
         {
             // [][]
             // [][]
@@ -93,199 +93,117 @@
         }
     ];
 
-    var timer;
-
-    var hiScore = 0, score = 0, speed = 0, removeNum = 0, blockNum = 0;
-
-    var block = {
-        r: 0,
-        c: 0,
-        shape: 0,
-        iBlock: 0,
-        iShape: 0,
-        color: 0
-    };
-    var nextBlock = 0;
-
-    var isGaming = false;
-    var isPaused = false;
-    var isMovable = false;
-    var isMuted = false;
-
-    var music = {
+    const MUSIC = {
         bgm: document.createElement('audio'),
         gameover: document.createElement('audio'),
         remove: document.createElement('audio')
     }
-    music.bgm.setAttribute('src', 'sounds/bgm_mario.mp3');
-    music.bgm.loop = true;
-    music.gameover.setAttribute('src', 'sounds/gameover.mp3');
-    music.remove.setAttribute('src', 'sounds/remove.mp3');
+    MUSIC.bgm.setAttribute('src', 'sounds/bgm_mario.mp3');
+    MUSIC.bgm.loop = true;
+    MUSIC.gameover.setAttribute('src', 'sounds/gameover.mp3');
+    MUSIC.remove.setAttribute('src', 'sounds/remove.mp3');
 
-    var toggleMute = function() {
-        if (isMuted) {
-            isMuted = false;
-            $("#muteIcon").attr('src', '../../octicons/svg/mute.svg');
-            $("#mute").removeClass("active");
-            if (isGaming) {
-                music.bgm.load();
-                music.bgm.play();
-            }
-        } else {
-            isMuted = true;
-            $("#muteIcon").attr('src', '../../octicons/svg/unmute.svg');
-            $("#mute").addClass("active");
-            music.bgm.pause();
-            music.gameover.pause();
-            music.remove.pause();
+    var hiScore = 0, score = 0, speed = 0, removeNum = 0, blockNum = 0;
+    var isGaming = false;
+    var isPaused = false;
+    var isMovable = false;
+    var isMuted = false;
+    var timer;
+    var nextBlock = 0;
+    var currentBlock = {
+        r: 0,
+        c: 0,
+        type: 0,
+        rotate: 0,
+        shape: function() {
+            return BLOCKS[this.type].shapes[this.rotate];
+        }
+    };
+
+    var drawBlock = function(r, c, block, rotate) {
+        var shape = BLOCKS[block].shapes[rotate];
+        var color = COLORS[BLOCKS[block].color];
+        for (let i = 0; i < shape.length; i++) {
+            $("#g" + (r + shape[i][0]) + "_" + (c + shape[i][1])).addClass(color).addClass("block");
         }
     }
 
-    var isColValid = function (c, shape) {
-        for (var i = 0; i < shape.length; i++) {
-            var ci = c + shape[i][1];
+    var cleanBlock = function () {
+        $(".block").removeClass(COLORS).removeClass("block");
+    }
+
+    var isBlockFilled = function (r, c, shape) {
+        for (let i = 0; i < shape.length; i++) {
+            let ri = r + shape[i][0];
+            if (ri < 0 || ri >= ROW_NUM) return false;
+            let ci = c + shape[i][1];
+            if (ci < 0 || ci >= COL_NUM) return false;
+            if (!$('#g' + ri + '_' + ci).hasClass("filled")) return false;
+        }
+        return true;
+    }
+
+    var findTop = function(c, shape) {
+        var rmax = 0;
+        for (let i = 0; i < shape.length; i++) {
+            if (shape[i][0] > rmax) rmax = shape[i][0];
+        }
+        for (let r = 0; r < ROW_NUM - rmax; r++) {
+            if (isBlockFilled(r, c, shape)) return r;
+        }
+        return ROW_NUM - rmax - 1;
+    }
+
+    var isValid = function(c, shape) {
+        for (let i = 0; i < shape.length; i++) {
+            let ci = c + shape[i][1];
             if (ci < 0 || ci >= COL_NUM) return false;
         }
         return true;
     }
 
-    var isBlockAllFilled = function () {
-        for (var i = 0; i < block.shape.length; i++) {
-            if (!$('#g' + (block.r + block.shape[i][0]) + '_' + (block.c + block.shape[i][1])).hasClass("filled")) return false;
-        }
-        return true;
+    var newBlock = function() {
+        currentBlock.c = INIT_C;
+        currentBlock.type = nextBlock;
+        currentBlock.rotate = 0;
+        currentBlock.r = findTop(INIT_C, currentBlock.shape())
+
+        drawCurrentBlock();
+        updateNext();
     }
 
-    var newNextBlock = function () {
+    var updateNext = function() {
         nextBlock = Math.floor(Math.random() * BLOCKS.length);
         //draw next block
-        $(".grid_next").removeClass(BG_COLORS);
+        $(".grid_next").removeClass(COLORS);
         var nextShape = BLOCKS[nextBlock].shapes[0];
         var nextColor = BLOCKS[nextBlock].color;
-        for (var i = 0; i < nextShape.length; i++) {
-            $('#n' + (nextShape[i][0]) + '_' + (nextShape[i][1])).addClass(BG_COLORS[nextColor]);
+        for (let i = 0; i < nextShape.length; i++) {
+            $('#n' + (nextShape[i][0]) + '_' + (nextShape[i][1])).addClass(COLORS[nextColor]);
         }
-    }
-
-    var cleanBlock = function () {
-        $(".block").removeClass(BG_COLORS[block.color]).removeClass("block");
-    }
-
-    var drawBlock = function () {
-        for (var i = 0; i < block.shape.length; i++) {
-            $('#g' + (block.r + block.shape[i][0]) + '_' + (block.c + block.shape[i][1])).addClass(BG_COLORS[block.color]).addClass("block");
-        }
-    }
-
-    //giving block.c ready, find proper block.r
-    var positionBlock = function () {
-        var rmin = 0, rmax = 0;
-        for (var i = 0; i < block.shape.length; i++) {
-            if (block.shape[i][0] < rmin) rmin = block.shape[i][0];
-            if (block.shape[i][0] > rmax) rmax = block.shape[i][0];
-        }
-        for (var ri = -rmin; ri < ROW_NUM - rmax; ri++) {
-            block.r = ri;
-            if (isBlockAllFilled()) break;
-        }
-    }
-
-    var newBlock = function () {
-        block.c = INIT_C;
-        block.shape = BLOCKS[nextBlock].shapes[0];
-        block.iBlock = nextBlock;
-        block.iShape = 0;
-        block.color = BLOCKS[nextBlock].color;
-
-        positionBlock();
-        drawBlock();
-
-        newNextBlock();
-    }
-
-    var gameover = function () {
-        isGaming = false;
-        $(document).off("keydown");
-        clearInterval(timer);
-        $("#gameover").show();
-        if (score > hiScore) {
-            hiScore = score;
-            $("#hi-score").text(hiScore);
-        }
-        if (!isMuted) {
-            music.bgm.pause();
-            music.gameover.play();
-        }
-    }
-
-    var growth = function () {
-        isMovable = false;
-        if ($("#row0").children().hasClass("filled")) {
-            gameover();
-            return;
-        }
-        var shouldMoveBlock = isBlockAllFilled();
-        if (!shouldMoveBlock) {
-            cleanBlock();
-        }
-        for (var ri = 0; ri < ROW_NUM - 1; ri++) {
-            for (var ci = 0; ci < COL_NUM; ci++) {
-                $('#g' + ri + '_' + ci).attr("class", $('#g' + (ri + 1) + '_' + ci).attr("class"));
-            }
-        }
-        $('#row' + (ROW_NUM - 1)).children().removeClass(BG_COLORS).removeClass("block").addClass("filled");
-        if (shouldMoveBlock) {
-            block.r--;
-        } else {
-            drawBlock();
-        }
-
-        isMovable = true;
-    }
-
-    var tryMoveBlock = function (deltaC) {
-        isMovable = false;
-        if (isColValid(block.c + deltaC, block.shape)) {
-            cleanBlock();
-            block.c += deltaC;
-            positionBlock();
-            drawBlock();
-        }
-        isMovable = true;
-    }
-
-    var tryRotateBlock = function () {
-        isMovable = false;
-        var newShape = (block.iShape + 1) % 4;
-        if (isColValid(block.c, BLOCKS[block.iBlock].shapes[newShape])) {
-            cleanBlock();
-            block.iShape = newShape;
-            block.shape = BLOCKS[block.iBlock].shapes[block.iShape];
-            positionBlock();
-            drawBlock();
-        }
-        isMovable = true;
     }
 
     var removeRow = function(r, callback) {
         if (!isMuted) {
-            music.remove.pause();
-            music.remove.load();
-            music.remove.play();
+            MUSIC.remove.pause();
+            MUSIC.remove.load();
+            MUSIC.remove.play();
         }
-        
+
         removeNum++;
         $("#remove").text(removeNum);
         score += REMOVE_LINE_SCORE;
         $("#score").text(score);
+
         //adjust speed
         if (speed < SPEEDS.length - 1 && Math.floor(removeNum / SPEED_THRESHOLD) > speed) {
             speed++;
             $("#speed").text(speed);
             resetTimer();
         }
+
         var c = 1;
-        var color = BG_COLORS[Math.floor(Math.random() * BG_COLORS.length)];
+        var color = COLORS[Math.floor(Math.random() * COLORS.length)];
         var cleanGrid = () => {
             if (c < COL_NUM) {
                 $('#g' + r + '_' + (c - 1)).removeClass(color);
@@ -296,12 +214,12 @@
                 $('#g' + r + '_' + (c - 1)).removeClass(color);
                 var ri = r;
                 while (ri > 0) {
-                    for (var ci = 0; ci < COL_NUM; ci++) {
+                    for (let ci = 0; ci < COL_NUM; ci++) {
                         $('#g' + ri + '_' + ci).attr("class", $('#g' + (ri - 1) + '_' + ci).attr("class"));
                     }
                     ri--;
                 }
-                $("#row0").children().removeClass(BG_COLORS).removeClass("filled");
+                $("#row0").children().removeClass(COLORS).removeClass("filled");
                 callback();
             }
         };
@@ -309,15 +227,44 @@
         setTimeout(cleanGrid, ANIMATE_DURATION);
     }
 
-    var tryOpenBlock = function () {
-        isPaused = true;
+    var raise = function() {
         isMovable = false;
-
-        if (!isBlockAllFilled()) {
-            isPaused = false;
-            isMovable = true;
+        if ($("#row0").children().hasClass("filled")) {
+            gameover();
             return;
         }
+        var shouldMoveBlock = isCurrentBlockFilled();
+        if (!shouldMoveBlock) {
+            cleanBlock();
+        }
+        for (let ri = 0; ri < ROW_NUM - 1; ri++) {
+            for (let ci = 0; ci < COL_NUM; ci++) {
+                $('#g' + ri + '_' + ci).attr("class", $('#g' + (ri + 1) + '_' + ci).attr("class"));
+            }
+        }
+        $('#row' + (ROW_NUM - 1)).children().removeClass(COLORS).removeClass("block").addClass("filled");
+        if (shouldMoveBlock) {
+            currentBlock.r--;
+        } else {
+            drawCurrentBlock();
+        }
+
+        isMovable = true;
+    }
+
+    var isCurrentBlockFilled = function() {
+        return isBlockFilled(currentBlock.r, currentBlock.c, currentBlock.shape());
+    }
+
+    var drawCurrentBlock = function() {
+        drawBlock(currentBlock.r, currentBlock.c, currentBlock.type, currentBlock.rotate);
+    }
+
+    var tryOpenBlock = function() {
+        if (!isCurrentBlockFilled()) return;
+
+        isPaused = true;
+        isMovable = false;
 
         $(".block").removeClass("filled");
         cleanBlock();
@@ -328,8 +275,9 @@
         $("#score").text(score);
 
         var rowsToRemove = [];
-        for (var r = -1, i = 0; i < block.shape.length; i++) {
-            var ri = block.r + block.shape[i][0];
+        var shape = currentBlock.shape();
+        for (let r = -1, i = 0; i < shape.length; i++) {
+            let ri = currentBlock.r + shape[i][0];
             if (ri === r) continue;
             r = ri;
             if (!$('#row' + r).children().hasClass("filled")) {
@@ -355,23 +303,33 @@
         }
     }
 
-    var resetTimer = function () {
-        clearInterval(timer);
-        timer = setInterval(() => {
-            if (isPaused) {
-                clearInterval(timer);
-                setTimeout(resetTimer, 1000);
-            } else {
-                growth();
-            }
-        }, SPEEDS[speed]);
+    var tryMoveBlock = function(deltaC) {
+        isMovable = false;
+        if (isValid(currentBlock.c + deltaC, currentBlock.shape())) {
+            cleanBlock();
+            currentBlock.c += deltaC;
+            currentBlock.r = findTop(currentBlock.c, currentBlock.shape());
+            drawCurrentBlock();
+        }
+        isMovable = true;
+    }
+
+    var tryRotateBlock = function() {
+        isMovable = false;
+        var rotate = (currentBlock.rotate + 1) % 4;
+        if (isValid(currentBlock.c, BLOCKS[currentBlock.type].shapes[rotate])) {
+            cleanBlock();
+            currentBlock.rotate = rotate;
+            currentBlock.r = findTop(currentBlock.c, currentBlock.shape());
+            drawCurrentBlock();
+        }
+        isMovable = true;
     }
 
     var keydownEvent = function(event) {
         if (!isMovable) {
             return false;
         }
-        isMovable = false;
         switch (event.which) {
             case 32: //space
                 tryOpenBlock();
@@ -387,17 +345,67 @@
                 break;
             case 40: //down
                 resetTimer();
-                growth();
+                raise();
                 break;
             default:
                 break;
         }
-        // isMovable = true;
         return false;
     }
 
-    var game = function () {
-        for (var r = ROW_NUM - 4; r < ROW_NUM; r++) {
+    var resetTimer = function() {
+        clearInterval(timer);
+        timer = setInterval(() => {
+            if (isPaused) {
+                clearInterval(timer);
+                setTimeout(resetTimer, 1000);
+            } else {
+                raise();
+            }
+        }, SPEEDS[speed]);
+    }
+
+    var gameover = function() {
+        isGaming = false;
+        $(document).off("keydown");
+        clearInterval(timer);
+        
+        $("#gameover").show();
+
+        if (score > hiScore) {
+            hiScore = score;
+            $("#hi-score").text(hiScore);
+        }
+
+        if (!isMuted) {
+            MUSIC.bgm.pause();
+            MUSIC.gameover.play();
+        }
+    }
+
+    var reset = function() {
+        $("#gameover").hide();
+
+        $(".grid").removeClass(COLORS).removeClass(["block", "filled"]);
+        $(".grid_next").removeClass(COLORS).removeClass("block");
+
+        score = 0;
+        speed = 0;
+        removeNum = 0;
+        blockNum = 0;
+
+        $("#score").text(score);
+        $("#speed").text(speed);
+        $("#remove").text(removeNum);
+        $("#blocks").text(blockNum);
+
+        MUSIC.gameover.pause();
+        MUSIC.bgm.pause();
+        MUSIC.bgm.load();
+    }
+
+    var game = function() {
+        for (let r = ROW_NUM - 4; r < ROW_NUM; r++) {
             $('#row' + r).children().addClass("filled");
         }
 
@@ -413,52 +421,49 @@
         isMovable = true;
 
         if (!isMuted) {
-            music.bgm.play();
+            MUSIC.bgm.play();
         }
     }
 
-    var reset = function () {
-        $("#gameover").hide();
-
-        clearInterval(timer);
-        $(".grid").removeClass(BG_COLORS).removeClass(["block", "filled"]);
-        $(".grid_next").removeClass(BG_COLORS).removeClass("block");
-
-        score = 0;
-        speed = 0;
-        removeNum = 0;
-        blockNum = 0;
-
-        $("#score").text(score);
-        $("#speed").text(speed);
-        $("#remove").text(removeNum);
-        $("#blocks").text(blockNum);
-
-        music.gameover.pause();
-        music.bgm.pause();
-        music.bgm.load();
+    var toggleMute = function() {
+        if (isMuted) {
+            isMuted = false;
+            $("#muteIcon").attr('src', '../../octicons/svg/mute.svg');
+            $("#mute").removeClass("active");
+            if (isGaming) {
+                MUSIC.bgm.load();
+                MUSIC.bgm.play();
+            }
+        } else {
+            isMuted = true;
+            $("#muteIcon").attr('src', '../../octicons/svg/unmute.svg');
+            $("#mute").addClass("active");
+            MUSIC.bgm.pause();
+            MUSIC.gameover.pause();
+            MUSIC.remove.pause();
+        }
     }
 
-    var main = function () {
+    var main = function() {
         $("#gameover").hide();
-        
-        for (var ri = 0; ri < ROW_NUM; ri++) {
-            var row = $('<div id="row' + ri + '" class="row no-gutters row-cols-' + COL_NUM + '"></div>').appendTo($('#field'));
-            for (var ci = 0; ci < COL_NUM; ci++) {
-                var grid = $('<div id="g' + ri + '_' + ci + '" class="grid col border border-light"></div>').appendTo(row);
+
+        for (let ri = 0; ri < ROW_NUM; ri++) {
+            let row = $('<div id="row' + ri + '" class="row no-gutters row-cols-' + COL_NUM + '"></div>').appendTo($('#field'));
+            for (let ci = 0; ci < COL_NUM; ci++) {
+                let grid = $('<div id="g' + ri + '_' + ci + '" class="grid col border border-light"></div>').appendTo(row);
                 grid.css('padding-bottom', (100 / COL_NUM) + '%');
             }
         }
 
-        for (var ri = 0; ri < 2; ri++) {
-            var row = $('<div class="row no-gutters row-cols-4"></div>').appendTo($('#next'));
-            for (var ci = 0; ci < 4; ci++) {
-                var grid = $('<div id="n' + ri + '_' + ci + '" class="grid_next col border border-light"></div>').appendTo(row);
+        for (let ri = 0; ri < 2; ri++) {
+            let row = $('<div class="row no-gutters row-cols-4"></div>').appendTo($('#next'));
+            for (let ci = 0; ci < 4; ci++) {
+                let grid = $('<div id="n' + ri + '_' + ci + '" class="grid_next col border border-light"></div>').appendTo(row);
                 grid.css('padding-bottom', '25%');
             }
         }
 
-        $("#start").on('click', () => {
+        $("#start").on("click", () => {
             reset();
             game();
         });
@@ -467,4 +472,4 @@
     }
 
     $(document).ready(main);
-})()
+})();
