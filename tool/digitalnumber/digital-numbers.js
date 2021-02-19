d7a08a1706cc5dbd84885b18a2593be5536d9280
ztgeng <zhtgeng@gmail.com>
2021-02-18 17:15:44 -0800

    Update digital numbers js

--- tool/digitalnumber/digital-numbers.js
+++ tool/digitalnumber/digital-numbers.js
@@ -1,53 +1,49 @@
+/**
+ * @version 0.10
+ * @author Geng
+ */
 
 (function() {
-    var get_lines = function(num) {
-        switch (num) {
-            case 0:
-                return [true, true, true, false, true, true, true];
-            case 1:
-                return [false, false, true, false, false, true, false];
-            case 2:
-                return [true, false, true, true, true, false, true];
-            case 3:
-                return [true, false, true, true, false, true, true];
-            case 4:
-                return [false, true, true, true, false, true, false];
-            case 5:
-                return [true, true, false, true, false, true, true];
-            case 6:
-                return [true, true, false, true, true, true, true];
-            case 7:
-                return [true, false, true, false, false, true, false];
-            case 8:
-                return [true, true, true, true, true, true, true];
-            case 9:
-                return [true, true, true, true, false, true, true];
-            default:
-                return [true, true, true, true, true, true, true];
-        }
-    }
+    const HEIGHT_DEFAULT = 25;
+    const COLOR_DEFAULT = "orange";
+    const LINES = [
+        [true, true, true, false, true, true, true], // 0
+        [false, false, true, false, false, true, false], // 1
+        [true, false, true, true, true, false, true], // 2
+        [true, false, true, true, false, true, true], // 3
+        [false, true, true, true, false, true, false], // 4
+        [true, true, false, true, false, true, true], // 5
+        [true, true, false, true, true, true, true], // 6
+        [true, false, true, false, false, true, false], // 7
+        [true, true, true, true, true, true, true], // 8
+        [true, true, true, true, false, true, true] // 9
+    ];
 
-    var digit_html = function(num = 0, height = 25, color = "orange") {
-        var lines = get_lines(num % 10);
+    var digit_html = function(num = 0, height = HEIGHT_DEFAULT, color = COLOR_DEFAULT) {
+        if (num < 0) return "" + num;
+        if (num > 9) return digit_html(Math.floor(num / 10)) + digit_html(num % 10);
+
+        var lines = LINES[num];
         var width = height * 0.55;
         var line_width = height * 0.44, line_height = height * 0.12;
-        var padding = height / 20;
-        padding = padding < 1 ? 1 : padding;
-        var line_str = `<div style="position: absolute; width: ${line_width}px; height: ${line_height}px;">
+        var margin_x = Math.max(height / 20, 1);
+        var line_str1 = `<div style="position: absolute; width: ${line_width}px; height: ${line_height}px;`;
+        var line_str2 = `">
             <div style="position: absolute; width: ${(line_width - line_height) * 0.94}px; height: 0; border-bottom: ${line_height / 2}px solid ${color}; border-left: ${line_height / 2}px solid transparent; border-right: ${line_height / 2}px solid transparent; top: 0; left: ${(line_width - line_height) * 0.03}px; box-sizing: content-box;"></div>
             <div style="position: absolute; width: ${(line_width - line_height) * 0.94}px; height: 0; border-top: ${line_height / 2}px solid ${color}; border-left: ${line_height / 2}px solid transparent; border-right: ${line_height / 2}px solid transparent; top: 50%; left: ${(line_width - line_height) * 0.03}px; box-sizing: content-box;"></div>
         </div>`;
 
-        var container = $("<div></div>").css({"width": `${width}px`, "height": `${height}px`, "position": "relative", "display": "inline-block", "margin": `0 ${padding}px`});
-        if (lines[0]) container.append($(line_str).css({"top": "0", "left": "50%", "transform": "translate(-50%, 0%)"}));
-        if (lines[1]) container.append($(line_str).css({"top": `${line_height / 2}px`, "left": "0", "transform": "rotate(90deg) translate(0%, -100%)", "transform-origin": "top left"}));
-        if (lines[2]) container.append($(line_str).css({"top": `${line_height / 2}px`, "left": "100%", "transform": "rotate(90deg) translate(0%, 0%)", "transform-origin": "top left"}));
-        if (lines[3]) container.append($(line_str).css({"top": `${line_width}px`, "left": "50%", "transform": "translate(-50%, 0%)"}));
-        if (lines[4]) container.append($(line_str).css({"top": `${line_width + line_height / 2}px`, "left": "0", "transform": "rotate(90deg) translate(0%, -100%)", "transform-origin": "top left"}));
-        if (lines[5]) container.append($(line_str).css({"top": `${line_width + line_height / 2}px`, "left": "100%", "transform": "rotate(90deg) translate(0%, 0%)", "transform-origin": "top left"}));
-        if (lines[6]) container.append($(line_str).css({"top": `${line_width * 2}px`, "left": "50%", "transform": "translate(-50%, 0%)"}));
+        var number_str = `<div style="width: ${width}px; height: ${height}px; position: relative; display: inline-block; margin: 0 ${margin_x}px;">`;
+        if (lines[0]) number_str += (line_str1 + `top: 0; left: 50%; transform: translate(-50%, 0%);` + line_str2);
+        if (lines[1]) number_str += (line_str1 + `top: ${line_height / 2}px; left: 0; transform: rotate(90deg) translate(0%, -100%); transform-origin: top left;` + line_str2);
+        if (lines[2]) number_str += (line_str1 + `top: ${line_height / 2}px; left: 100%; transform: rotate(90deg) translate(0%, 0%); transform-origin: top left;` + line_str2);
+        if (lines[3]) number_str += (line_str1 + `top: ${line_width}px; left: 50%; transform: translate(-50%, 0%);` + line_str2);
+        if (lines[4]) number_str += (line_str1 + `top: ${line_width + line_height / 2}px; left: 0; transform: rotate(90deg) translate(0%, -100%); transform-origin: top left;` + line_str2);
+        if (lines[5]) number_str += (line_str1 + `top: ${line_width + line_height / 2}px; left: 100%; transform: rotate(90deg) translate(0%, 0%); transform-origin: top left;` + line_str2);
+        if (lines[6]) number_str += (line_str1 + `top: ${line_width * 2}px; left: 50%; transform: translate(-50%, 0%);` + line_str2);
+        number_str += `</div>`;
 
-        return container;
+        return number_str;
     }
 
     $(".digital-numbers-js").css({"display": "inline-block"});
@@ -62,7 +58,7 @@
                 if (isNaN(num)) {
                     newHtml += text[i];
                 } else {
-                    newHtml += digit_html(num, div.data("height"), div.data("color"))[0].outerHTML;
+                    newHtml += digit_html(num, div.data("height"), div.data("color"));
                 }
             }
             div.html(newHtml)
