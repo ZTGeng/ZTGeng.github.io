301eff49c914bf146f965a6a48bab8f54e612f1f
ztgeng <zhtgeng@gmail.com>
2023-11-16 22:24:16 -0800

    New home page and some other changes

--- tool/js/novelfilterdata.js
+++ tool/js/novelfilterdata.js
@@ -2,163 +2,163 @@ var demoData = {
     items: [
         {
             name: "成为冒险家吧！",
-            imgUrl: "https://i.pinimg.com/564x/57/4d/6a/574d6ae15748a269c3304a164f9cd398.jpg",
+            imgUrl: "images/novelfilter/574d6ae15748a269c3304a164f9cd398.jpg",
             url: "https://www.esjzone.cc/detail/1555318723.html",
             tags: ["小说", "漫画", "单男主", "现实世界异化"]
         },
         {
             name: "在异世界迷宫开后宫",
-            imgUrl: "https://i.pinimg.com/originals/98/89/b5/9889b5f53c69ba2194d566092efb12bb.jpg",
+            imgUrl: "images/novelfilter/9889b5f53c69ba2194d566092efb12bb.jpg",
             url: "https://www.esjzone.cc/detail/1543387217.html",
             tags: ["小说", "漫画", "单男主", "异世界穿越", "奴隶", "后宫", "18x"]
         },
         {
             name: "这个勇者明明超强却过分慎重",
-            imgUrl: "https://i.pinimg.com/564x/da/d6/b1/dad6b1faf11a5573d9e94f072f3031dc.jpg",
+            imgUrl: "images/novelfilter/dad6b1faf11a5573d9e94f072f3031dc.jpg",
             url: "https://www.esjzone.cc/detail/1546223660.html",
             tags: ["小说", "漫画", "动画", "多主角", "异世界穿越"]
         },
         {
             name: "原世界第一玩家的小号练成日记",
-            imgUrl: "https://i.pinimg.com/564x/17/8e/ac/178eac0c59bf2d044e699894d794cf3c.jpg",
+            imgUrl: "images/novelfilter/178eac0c59bf2d044e699894d794cf3c.jpg",
             url: "https://www.esjzone.cc/detail/1548946964.html",
             tags: ["小说", "漫画", "单男主", "异世界穿越", "后宫"]
         },
         {
             name: "我不是说了能力要平均值么",
-            imgUrl: "https://i.pinimg.com/564x/91/82/50/918250c8386cb3a720477a7a785eb896.jpg",
+            imgUrl: "images/novelfilter/918250c8386cb3a720477a7a785eb896.jpg",
             url: "https://www.esjzone.cc/detail/1544163514.html",
             tags: ["小说", "漫画", "动画", "单女主", "异世界原住民"]
         },
         {
             name: "由于世界魔物满载，因此想要随意过活",
-            imgUrl: "https://i.pinimg.com/564x/f4/11/fc/f411fc625c6a1bd9a96b26bb6da65259.jpg",
+            imgUrl: "images/novelfilter/f411fc625c6a1bd9a96b26bb6da65259.jpg",
             url: "https://www.esjzone.cc/detail/1543764918.html",
             tags: ["小说", "漫画", "单男主", "现实世界异化"]
         },
         {
             name: "世界最强后卫",
-            imgUrl: "https://i.pinimg.com/564x/65/ff/41/65ff413ddab342ef469a09c0a9437a80.jpg",
+            imgUrl: "images/novelfilter/65ff413ddab342ef469a09c0a9437a80.jpg",
             url: "https://www.esjzone.cc/detail/1545938213.html",
             tags: ["小说", "漫画", "单男主", "异世界穿越", "后宫"]
         },
         {
             name: "异世界悠闲农家",
-            imgUrl: "https://i.pinimg.com/564x/10/db/3d/10db3d662f71efd25273b2fd72b02f2a.jpg",
+            imgUrl: "images/novelfilter/10db3d662f71efd25273b2fd72b02f2a.jpg",
             url: "https://www.esjzone.cc/detail/1545922563.html",
             tags: ["小说", "漫画", "单男主", "异世界穿越", "后宫"]
         },
         {
             name: "熊熊勇闯异世界",
-            imgUrl: "https://i.pinimg.com/564x/ec/12/6c/ec126c2388bd9cd605b9109da2026bbd.jpg",
+            imgUrl: "images/novelfilter/ec126c2388bd9cd605b9109da2026bbd.jpg",
             url: "https://www.esjzone.cc/detail/1544080369.html",
             tags: ["小说", "漫画", "动画", "单女主", "异世界穿越"]
         },
         {
             name: "异世界回归勇者在现代无双",
-            imgUrl: "https://i.pinimg.com/564x/9e/5c/cb/9e5ccb509631dd4a441e8b202a5e1397.jpg",
+            imgUrl: "images/novelfilter/9e5ccb509631dd4a441e8b202a5e1397.jpg",
             url: "https://www.esjzone.cc/detail/1554054936.html",
             tags: ["小说", "漫画", "单男主", "现实世界异化"]
         },
         {
             name: "无职转生",
-            imgUrl: "https://i.pinimg.com/564x/c7/08/06/c708067d34e73f0171f572909d2075a3.jpg",
+            imgUrl: "images/novelfilter/c708067d34e73f0171f572909d2075a3.jpg",
             url: "https://www.esjzone.cc/detail/1543764122.html",
             tags: ["小说", "漫画", "单男主", "异世界穿越"]
         },
         {
             name: "被勇者队伍开除的驭兽使，邂逅了最强种的猫耳少女",
-            imgUrl: "https://i.pinimg.com/564x/8d/77/b9/8d77b9f48553eb688aaac99c15c54dc5.jpg",
+            imgUrl: "images/novelfilter/8d77b9f48553eb688aaac99c15c54dc5.jpg",
             url: "https://www.esjzone.cc/detail/1557158219.html",
             tags: ["小说", "漫画", "单男主", "异世界原住民", "后宫", "逆袭"]
         },
         {
             name: "怕痛少女的防御特化",
-            imgUrl: "https://i.pinimg.com/564x/99/01/64/9901641b1d831be5ee068391a7b5d7c5.jpg",
+            imgUrl: "images/novelfilter/9901641b1d831be5ee068391a7b5d7c5.jpg",
             url: "https://www.esjzone.cc/detail/1546618252.html",
             tags: ["小说", "漫画",  "单女主", "异世界穿越"]
         },
         {
             name: "身为暗杀者的我明显比勇者还强",
-            imgUrl: "https://i.pinimg.com/564x/e8/f8/36/e8f836cd6633c92954ae188546de888e.jpg",
+            imgUrl: "images/novelfilter/e8f836cd6633c92954ae188546de888e.jpg",
             url: "https://www.esjzone.cc/detail/1546316644.html",
             tags: ["小说", "漫画", "单男主", "异世界穿越", "全班穿越"]
         },
         {
             name: "关于我转生成为史莱姆的那件事",
-            imgUrl: "https://i.pinimg.com/564x/82/53/f1/8253f1373c421114953b7addf1ca9196.jpg",
+            imgUrl: "images/novelfilter/8253f1373c421114953b7addf1ca9196.jpg",
             url: "https://www.esjzone.cc/detail/1543763093.html",
             tags: ["小说", "漫画", "动画", "单男主", "异世界穿越"]
         },
         {
             name: "平凡职业成就世界最强",
-            imgUrl: "https://i.pinimg.com/564x/bb/06/d8/bb06d83fb3609f65dec8b3f8b0186315.jpg",
+            imgUrl: "images/novelfilter/bb06d83fb3609f65dec8b3f8b0186315.jpg",
             url: "https://www.esjzone.cc/detail/1543763817.html",
             tags: ["小说", "漫画", "单男主", "异世界穿越", "全班穿越", "逆袭"]
         },
         {
             name: "独自一人的异世界攻略",
-            imgUrl: "https://i.pinimg.com/564x/03/6a/fd/036afdd91483d37540bbd0b7e92d3d56.jpg",
+            imgUrl: "images/novelfilter/036afdd91483d37540bbd0b7e92d3d56.jpg",
             url: "https://www.esjzone.cc/detail/1551548101.html",
             tags: ["小说", "漫画", "单男主", "异世界穿越", "全班穿越"]
         },
         {
             name: "最强的我最终蹂躏一切",
-            imgUrl: "https://i.pinimg.com/564x/38/b1/86/38b186e95b6452c0104ac6d7986600b1.jpg",
+            imgUrl: "images/novelfilter/38b186e95b6452c0104ac6d7986600b1.jpg",
             url: "https://www.esjzone.cc/detail/1552200797.html",
             tags: ["小说", "漫画", "单男主", "异世界穿越", "全班穿越", "逆袭", "复仇"]
         },
         {
             name: "等级只有1级但固有技能是最强",
-            imgUrl: "https://i.pinimg.com/564x/52/6b/f1/526bf126a6a51fb99edb6b01ac3a7cb1.jpg",
+            imgUrl: "images/novelfilter/526bf126a6a51fb99edb6b01ac3a7cb1.jpg",
             url: "https://www.esjzone.cc/detail/1546342666.html",
             tags: ["小说", "漫画", "单男主", "异世界穿越", "逆袭"]
         },
         {
             name: "世界最强暗杀者被转生为异世界贵族",
-            imgUrl: "https://i.pinimg.com/564x/07/c0/3c/07c03cdeb350e2a707f42925a0878f0e.jpg",
+            imgUrl: "images/novelfilter/07c03cdeb350e2a707f42925a0878f0e.jpg",
             url: "https://www.esjzone.cc/detail/1548762549.html",
             tags: ["小说", "漫画", "单男主", "异世界穿越"]
         },
         {
             name: "D Genesis 迷宫出现三年后",
-            imgUrl: "https://images-na.ssl-images-amazon.com/images/I/51WDpVFDGDL._SX350_BO1,204,203,200_.jpg",
+            imgUrl: "images/novelfilter/51WDpVFDGDL._SX350_BO1,204,203,200_.jpg",
             url: "https://www.esjzone.cc/detail/1563635750.html",
             tags: ["小说", "单男主", "现实世界异化"]
         },
         {
             name: "为了养老金去异界存八万金",
-            imgUrl: "https://i.pinimg.com/564x/d9/18/5a/d9185a8b3931138953a52fdea8e85654.jpg",
+            imgUrl: "images/novelfilter/d9185a8b3931138953a52fdea8e85654.jpg",
             url: "https://www.esjzone.cc/detail/1545652646.html",
             tags: ["小说", "漫画",  "单女主", "异世界穿越"]
         },
         {
             name: "其实，我乃最强？",
-            imgUrl: "https://i.pinimg.com/564x/2e/75/3e/2e753ec2fbbbf55b1edf753f2ab02dd6.jpg",
+            imgUrl: "images/novelfilter/2e753ec2fbbbf55b1edf753f2ab02dd6.jpg",
             url: "https://www.esjzone.cc/detail/1560850775.html",
             tags: ["小说", "漫画", "单男主", "异世界穿越", "逆袭"]
         },
         {
             name: "Re：從零開始的異世界生活",
-            imgUrl: "https://i.pinimg.com/564x/36/95/41/3695413c5b210a8f3427ca36a694f99a.jpg",
+            imgUrl: "images/novelfilter/3695413c5b210a8f3427ca36a694f99a.jpg",
             url: "https://www.esjzone.cc/detail/1585261417.html",
             tags: ["小说", "漫画", "动画", "单男主", "异世界穿越"]
         },
         {
             name: "在地下城寻求邂逅是否搞错了什么",
-            imgUrl: "https://m.media-amazon.com/images/I/51awckI5cUL._SY346_.jpg",
+            imgUrl: "images/novelfilter/51awckI5cUL._SY346_.jpg",
             url: "https://baike.baidu.com/item/%E5%9C%A8%E5%9C%B0%E4%B8%8B%E5%9F%8E%E5%AF%BB%E6%B1%82%E9%82%82%E9%80%85%E6%98%AF%E5%90%A6%E6%90%9E%E9%94%99%E4%BA%86%E4%BB%80%E4%B9%88/13008106",
             tags: ["小说", "漫画", "动画", "单男主", "异世界原住民", "逆袭"]
         },
         {
             name: "魔法科高中的劣等生",
-            imgUrl: "https://i.pinimg.com/564x/98/1c/40/981c404ff9102d0d776aefd84326051e.jpg",
+            imgUrl: "images/novelfilter/981c404ff9102d0d776aefd84326051e.jpg",
             url: "https://www.esjzone.cc/detail/1543762490.html",
             tags: ["小说", "漫画", "动画", "单男主", "现实世界异化", "逆袭"]
         },
         {
             name: "OVERLORD不死者之王",
-            imgUrl: "https://i.pinimg.com/564x/20/c7/a9/20c7a974813617e6e9afe2b1682160e9.jpg",
+            imgUrl: "images/novelfilter/20c7a974813617e6e9afe2b1682160e9.jpg",
             url: "https://www.esjzone.cc/detail/1543762901.html",
             tags: ["小说", "漫画", "动画", "单男主", "异世界穿越"]
         }
